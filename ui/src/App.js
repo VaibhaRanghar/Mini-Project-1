b0efbadf9c68ee216c71d28f5a9ee7a358598fe8
VaibhavRanghar <vaibhavranghar150@gmail.com>
2024-02-19 22:46:24 +0530

    Added desc

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to our new PROJECT!</h1>
+      <p>This project is used to increase our existing knowledge about project management!</p>
     </div>
   );
 }
