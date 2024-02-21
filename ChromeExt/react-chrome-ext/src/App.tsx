57c7f3cd51a6e470216aac4fe908fb282b234c67
VaibhavRanghar <vaibhavranghar150@gmail.com>
2024-02-21 21:42:47 +0530

    A working chrome extension build

---
+++ ChromeExt/react-chrome-ext/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World from App.tsx from react app!</h1>
+    </div>
+  );
+}
+
+export default App;
